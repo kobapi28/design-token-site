aa959f1f3ad8daf1f75548cc157553a6250c16e7
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-30 10:19:05 +0900

    feat: webpack周りのプラグインが動作していることを確認

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+// cssファイルとして読み込めるように
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+// HTMLファイルのビルド
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './public',
+    watchFiles: ['./src/index.html']
+  },
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: `${__dirname}/public/index.html`,
+      inject: 'body'
+    })
+  ]
+}
